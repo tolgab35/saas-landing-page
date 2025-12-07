dd07e48c10679cb323be300a9a641fe7dd14162a
Tolga Boz <tolgaboz350@gmail.com>
2025-12-07 23:43:33 +0300

    feat: update App component with new preview section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import heroImg from "./assets/hero-section.gif";
 import firmsImg from "./assets/firms.png";
+import previewImg from "./assets/preview.png";
 
 function App() {
   return (
     <div className="bg-[#2B2B2B] min-h-screen">
       {/* HEADER */}
-      <header className="max-w-[1280px] mx-auto px-6 py-6 flex items-center justify-between">
+      <header className="max-w-[1280px] mx-auto py-6 flex items-center justify-between">
         <div className="text-2xl font-semibold text-white">Wallet</div>
 
         <div className="flex items-center gap-4">
@@ -21,7 +22,7 @@ function App() {
 
       {/* HERO */}
       <section>
-        <div className="max-w-[1280px] mx-auto px-6 py-16 flex flex-col md:flex-row items-center gap-16">
+        <div className="max-w-[1280px] mx-auto  py-16 flex flex-col md:flex-row items-center gap-16">
           {/* LEFT */}
           <div className="flex-1 flex flex-col gap-12">
             <h1 className="text-6xl font-semibold text-white leading-snug">
@@ -51,8 +52,17 @@ function App() {
 
       {/* FIRMS */}
       <section>
-        <div className="max-w-[1280px] mx-auto px-12 py-16 flex items-center justify-between gap-8 border-t-2 border-b-2 border-white/10">
+        <div className="max-w-[1280px] mx-auto px-16 py-16 flex items-center justify-between gap-8 border-t-2 border-b-2 border-white/10">
           <img src={firmsImg} alt="Firms" />
+        </div>
+      </section>
+
+      {/* PREVIEW */}
+      <section>
+        <div className="max-w-[1280px] mx-auto py-16">
+          <div className="bg-[#FDF5DF] rounded-[20px] flex items-center justify-center">
+            <img src={previewImg} alt="Preview" />
+          </div>
         </div>
       </section>
     </div>
